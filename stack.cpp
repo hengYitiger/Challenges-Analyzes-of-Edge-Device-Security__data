cb4c298f0f5d078e3dfa50bb63a4c87ed50b1655
hengYitiger <837875067@qq.com>
2021-05-26 15:33:08 +0800

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,47 @@
+//
+//  stack0.c
+//  
+//
+//  Created by  on .
+//
+//
+
+#include <iostream>   
+using namespace std;   
+
+int main(int argc, const char *artv[])   
+{   
+
+ volatile int modified;
+    char buffer[64];
+    modified = 0;
+    
+	cout << "input buffer: " << endl;   
+    cin >> buffer;   
+    //gets(buffer);
+    
+    if (modified != 0) {
+        
+		cout << "You have changed the 'modified' variables!\n" << endl;   
+		
+    } else {
+        
+		cout << "Try again?!\n " << endl;   
+
+
+	
+    }
+
+
+
+return 0;   
+} 
+
+
+
+
+
+
+
+
+
